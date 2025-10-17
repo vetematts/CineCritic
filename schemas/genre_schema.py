50aed61a289d4c2388bda63910d31d075e59fa2d
vetematts <09_chitin_cubicle@icloud.com>
2025-10-17 12:52:37 +1000

    Implement Genre schema with validation (name required, length limits)

--- schemas/genre_schema.py
+++ schemas/genre_schema.py
@@ -1,8 +1,14 @@
-from marshmallow import Schema, fields, validate
+from marshmallow import Schema, fields, validate, pre_load
 
 class GenreCreateSchema(Schema):
     name = fields.String(required=True, validate=validate.Length(min=1, max=50))
 
+    @pre_load
+    def strip_name(self, data, **kwargs):
+        if 'name' in data and isinstance(data['name'], str):
+            data['name'] = data['name'].strip()
+        return data
+
 class GenreSchema(Schema):
-    id = fields.Integer()
+    id = fields.Integer(dump_only=True)
     name = fields.String()
