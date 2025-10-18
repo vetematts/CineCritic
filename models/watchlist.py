9c5958b5d06be470272c86d80f0efba36e3a34d5
vetematts <09_chitin_cubicle@icloud.com>
2025-10-18 13:21:00 +1000

    watchlist: fix duplicate fields

--- models/watchlist.py
+++ models/watchlist.py
@@ -3,16 +3,6 @@ from extensions import db
 class Watchlist(db.Model):
     __tablename__ = "watchlist_entries"
 
-    user_id = db.Column(
-        db.Integer,
-        db.ForeignKey("users.id", ondelete="CASCADE"),
-        primary_key=True
-    )
-    film_id = db.Column(
-        db.Integer,
-        db.ForeignKey("films.id", ondelete="CASCADE"),
-        primary_key=True
-    )
     added_at = db.Column(db.DateTime, server_default=db.func.now())
 
 user_id = db.Column(db.Integer, db.ForeignKey("users.id", ondelete="CASCADE"), primary_key=True)
