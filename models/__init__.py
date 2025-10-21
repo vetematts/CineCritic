85077686bbfdc4118ae105b744b34590e7047f8e
vetematts <09_chitin_cubicle@icloud.com>
2025-10-21 13:37:30 +1000

    Add docstring to models/__init__.py explaining import order dependencies

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,10 @@
+"""
+Models package initialisation.
+
+Ordering note:
+- Some models depend on others being defined first.
+- For example, Review requires User and Film to exist before it can be imported.
+"""
 from .users import User
 from .film import Film
 from .genre import Genre
