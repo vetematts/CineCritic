478e8d9cbc2d3b1c91efdff023379a2d17d96175
vetematts <09_chitin_cubicle@icloud.com>
2025-10-21 13:43:39 +1000

    Add docstring to schemas/__init__.py explaining import order dependencies

--- schemas/__init__.py
+++ schemas/__init__.py
@@ -1,3 +1,9 @@
+"""
+Schema imports for CineCritic.
+
+- Defines import order so dependent schemas load correctly
+- For example, ReviewSchema requires User and Film schemas
+"""
 from .film_schema import FilmCreateSchema, FilmSchema
 from .review_schema import ReviewCreateSchema, ReviewSchema
 from .genre_schema import GenreSchema
